347d36d0755959e52cdef9543cf6c548fceefa05
yaniv-cb4 <yanivn@c-b4.com>
2018-03-20 15:37:23 +0200

    Remove unused resources

--- databases/initdb/src/main/resources/bin/init-unifiedpush-db.sh
+++ databases/initdb/src/main/resources/bin/init-unifiedpush-db.sh
@@ -21,16 +21,12 @@ die() {
 while [ -n "$1" ]; do
     v="${1#*=}"
     case "$1" in
-        --database=*)
-            export DATABASE="${v}"
-            ;;
         --config-path=*)
             export CONFIG="${v}"
             ;;
         --help|*)
                 cat <<__EOF__
 Usage: $0
-        --database=database - Database name
         --config-path=path  - Path for -Daerobase.config.dir param - Default /tmp/db.properties
 __EOF__
         exit 1
@@ -38,12 +34,9 @@ __EOF__
     shift
 done
 
-[ -z "${DATABASE}" ] && die "Missing database! Usage: ./init-unifiedpush-db.sh database name"
 [ -z "${CONFIG}" ] && export CONFIG=/tmp/db.properties
 
 ${JAVA_HOME}/bin/java ${DEBUG_OPTS} \
         -Daerobase.config.dir=${CONFIG} \
-        -Dorg.jboss.aerogear.unifiedpush.initdb.database=${DATABASE} \
         -cp "../lib/*" \
-        org.jboss.aerogear.unifiedpush.DBMaintenance \
-        "${DATABASE}"
+        org.jboss.aerogear.unifiedpush.DBMaintenance
