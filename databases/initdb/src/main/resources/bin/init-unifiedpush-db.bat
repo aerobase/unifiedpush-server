4b3452f3a74dbabcdf70f94dab7d8db244a5227b
Yaniv Marom Nachumi <yaniv@aerobase.io>
2018-07-21 22:33:22 +0300

    Revert usage to power shell

--- databases/initdb/src/main/resources/bin/init-unifiedpush-db.bat
+++ databases/initdb/src/main/resources/bin/init-unifiedpush-db.bat
@@ -1,11 +1,11 @@
-@echo off
+@ECHO OFF
 IF %1.==. GOTO No1
 set CONFIG=%1
 
 REM set debug parameters
 REM set DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y"
 
-@powershell -Command "java -cp \"..\lib\*\" %DEBUG_OPTS% \"-Daerobase.config.dir=%CONFIG%\" org.jboss.aerogear.unifiedpush.DBMaintenance"
+@powershell -Command "java -cp '..\lib\*' %DEBUG_OPTS% \"-Daerobase.config.dir=%CONFIG%\" org.jboss.aerogear.unifiedpush.DBMaintenance"
 
 GOTO End1
 
