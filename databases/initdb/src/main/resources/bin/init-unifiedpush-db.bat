9188f6e655de701dfa69fddcfa807313e9d3bcbf
Yaniv Marom Nachumi <yaniv@aerobase.io>
2018-07-22 00:09:06 +0300

    Fix initdb under windows server 2016

--- databases/initdb/src/main/resources/bin/init-unifiedpush-db.bat
+++ databases/initdb/src/main/resources/bin/init-unifiedpush-db.bat
@@ -5,7 +5,7 @@ set CONFIG=%1
 REM set debug parameters
 REM set DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y"
 
-@powershell -Command "java -cp '..\lib\*' %DEBUG_OPTS% \"-Daerobase.config.dir=%CONFIG%\" org.jboss.aerogear.unifiedpush.DBMaintenance"
+java -cp ..\lib\* %DEBUG_OPTS% "-Daerobase.config.dir=%CONFIG%" org.jboss.aerogear.unifiedpush.DBMaintenance > initdb-java.log 2>&1
 
 GOTO End1
 
