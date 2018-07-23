b2bc88148c39c449cb198e0ee1099bc446890a25
Yaniv Marom Nachumi <yaniv@aerobase.io>
2018-07-23 21:14:58 +0300

    Use JAVA_HOME for schema creation

--- databases/initdb/src/main/resources/bin/init-unifiedpush-db.bat
+++ databases/initdb/src/main/resources/bin/init-unifiedpush-db.bat
@@ -5,7 +5,7 @@ set CONFIG=%1
 REM set debug parameters
 REM set DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y"
 
-java -cp ..\lib\* %DEBUG_OPTS% "-Daerobase.config.dir=%CONFIG%" org.jboss.aerogear.unifiedpush.DBMaintenance > initdb-java.log 2>&1
+"%JAVA_HOME%\bin\java" -cp ..\lib\* %DEBUG_OPTS% "-Daerobase.config.dir=%CONFIG%" org.jboss.aerogear.unifiedpush.DBMaintenance > initdb-java.log 2>&1
 
 GOTO End1
 
